53ab4ebe7de25d32d154654b792f1e8b2c3ac179
wyekings <wyekings@outlook.com>
2024-01-17 22:59:10 +0800

    Add splash screen

--- android/app/src/main/kotlin/com/wyekings/wan/MainActivity.kt
+++ android/app/src/main/kotlin/com/wyekings/wan/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.wyekings.wan
 
 import io.flutter.embedding.android.FlutterActivity
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen().apply { setKeepOnScreenCondition { true } }
+        super.onCreate(savedInstanceState)
+        hideSplashScreen(splashScreen)
+    }
+
+    private fun hideSplashScreen(splashScreen: SplashScreen) {
+        window.decorView.post { splashScreen.setKeepOnScreenCondition { false } }
+    }
 }
