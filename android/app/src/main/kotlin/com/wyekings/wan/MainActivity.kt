37073e826c02efe06f13a5fb64236c302b4d62f7
wyekings <wyekings@outlook.com>
2024-02-29 11:42:15 +0800

    Apply light status bar style for splash

--- android/app/src/main/kotlin/com/wyekings/wan/MainActivity.kt
+++ android/app/src/main/kotlin/com/wyekings/wan/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.wyekings.wan
 
+import android.app.Activity
 import io.flutter.embedding.android.FlutterActivity
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -10,6 +11,8 @@ class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen().apply { setKeepOnScreenCondition { true } }
         super.onCreate(savedInstanceState)
-        window.decorView.post { splashScreen.setKeepOnScreenCondition { false } }
+        window.decorView.post {
+            splashScreen.setKeepOnScreenCondition { false }
+        }
     }
 }
